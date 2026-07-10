1d4de4385e733c10394ed3769664d3c9fca30a4a
Mohammad Feroz Shikder <53492992+skpabel@users.noreply.github.com>
2026-07-10 19:03:57 +0600

    Initial commit - Laravel Somiti Management System

---
+++ app/Livewire/User/Notice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+#[Layout('components.layouts.guest')]
+class Notice extends Component
+{
+    public $member;
+
+    public function mount()
+    {
+        $this->member = auth()->user()->member;
+
+        if (!$this->member) {
+            return redirect()->route('logout');
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.user.notice');
+    }
+}
