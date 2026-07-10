995b86fda8b1e6f89730f8a3be9e1f41c0d63be3
Mohammad Feroz Shikder <53492992+skpabel@users.noreply.github.com>
2026-07-10 19:04:41 +0600

    Initial commit - Laravel Somiti Management System

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // Load helper functions
+        if (file_exists(app_path('helpers.php'))) {
+            require_once app_path('helpers.php');
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
